0f98f7184749e84901e248738f55b808ab5fb4c2
ee15btech11039 <ee15btech11039@iith.ac.in>
2017-06-01 14:34:59 -0400

    idinput function

---
+++ signalgenerator.m
@@ -0,0 +1,20 @@
+%test case
+
+u1=randn(50,2)
+band11=[0 0.5]
+nu1=2
+
+u2=randn(200,4)
+band21=[0 1]
+nu2=4
+
+u3=randn(500,3)
+band31=[0.2 0.7]
+nu3=3
+
+u4=randn(2000,2)
+band41=[0.5 1]
+nu4=2
+u5=randn(2500,2)
+band51=[0.15 0.6]
+nu5=2
